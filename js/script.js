f53f453b4de33ef6c9ce534cd8bb83d58f535cb9
nan <sss@qq.com>
2020-01-02 16:28:11 +0800

    ui简约版

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var socket = io('ws://localhost:3000');
+var local = new Local(socket);
+// local.start();
+var remote = new Remote(socket);
+// remote.bindEvents();
+socket.on('waiting',function(str) {
+    document.getElementById('waiting').innerHTML = str;
+})
